cb6eb404c3327ecd62099543bbd61f5f9d7ab697
inesppino <inesp.pino@gmail.com>
2020-04-25 14:46:45 +0200

    feat(bio):merge

--- _src/assets/js/bio.js
+++ _src/assets/js/bio.js
@@ -1,11 +1,36 @@
 'use strict';
 
 const skills = document.querySelector('#skills-btn');
-function showSkills(){
+function toggleSkills(){
+  const timelineContainer = document.querySelector('.timeline');
+  if(!timelineContainer.classList.contains('hide')){
+    timelineContainer.classList.add('hide');
+    const main = document.getElementsByTagName('main');
+    main[0].style.rowGap = '0rem';
+  }
   const skillsContainer = document.querySelector('.skills-container');
   skillsContainer.classList.toggle('hide');
 }
-skills.addEventListener('click', showSkills);
+skills.addEventListener('click', toggleSkills);
+
+const timeline = document.querySelector('#timeline-btn');
+function toggleTimeline(){
+  const skillsContainer = document.querySelector('.skills-container');
+  if(!skillsContainer.classList.contains('hide')){
+    skillsContainer.classList.add('hide');
+  }
+  const timelineContainer = document.querySelector('.timeline');
+  if(timelineContainer.classList.contains('hide')) {
+    const main = document.getElementsByTagName('main');
+    main[0].style.rowGap = '10rem';
+    timelineContainer.classList.remove('hide');
+  } else {
+    const main = document.getElementsByTagName('main');
+    main[0].style.rowGap = '0rem';
+    timelineContainer.classList.add('hide');
+  }
+}
+timeline.addEventListener('click', toggleTimeline);
 
 const profilePic = document.querySelector('.profile-pic');
 
